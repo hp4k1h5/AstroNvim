74e7271dd369ee38ccb1983242f931399610384d
bob <robertwalks@gmail.com>
2022-12-30 17:34:23 -0500

    edit: plugins

--- init.lua
+++ init.lua
@@ -1,21 +1,244 @@
-local impatient_ok, impatient = pcall(require, "impatient")
-if impatient_ok then impatient.enable_profile() end
+local config = {
+  updater = { channel = "nightly", auto_reload = true },
 
-for _, source in ipairs {
-  "core.utils",
-  "core.options",
-  "core.bootstrap",
-  "core.diagnostics",
-  "core.autocmds",
-  "core.mappings",
-  "configs.which-key-register",
-} do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+  colorscheme = "default_theme",
+
+  header = {
+    " █████  ███████ ████████ ██████   ██████",
+    "██   ██ ██         ██    ██   ██ ██    ██",
+    "███████ ███████    ██    ██████  ██    ██",
+    "██   ██      ██    ██    ██   ██ ██    ██",
+    "██   ██ ███████    ██    ██   ██  ██████",
+    " ",
+    "    ███    ██ ██    ██ ██ ███    ███",
+    "    ████   ██ ██    ██ ██ ████  ████",
+    "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+    "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+    "    ██   ████   ████   ██ ██      ██",
+  },
+
+  -- Default theme configuration
+  default_theme = {
+    colors = {
+      fg = "#abbfcf",
+      bg = "#222222",
+      -- bg = "NONE",
+    },
+    highlights = function(hl)
+      local C = require "default_theme.colors"
+      hl.Normal = { fg = C.fg, bg = C.bg }
+      hl.DiagnosticError.italic = true
+      hl.DiagnosticHint.italic = true
+      hl.DiagnosticInfo.italic = true
+      hl.DiagnosticWarn.italic = true
+
+      return hl
+    end,
+    -- enable or disable highlighting for extra plugins
+    plugins = {
+      aerial = true,
+      beacon = false,
+      bufferline = true,
+      cmp = true,
+      dashboard = true,
+      highlighturl = true,
+      hop = false,
+      indent_blankline = true,
+      lightspeed = false,
+      ["neo-tree"] = true,
+      notify = true,
+      ["nvim-tree"] = false,
+      ["nvim-web-devicons"] = true,
+      rainbow = true,
+      symbols_outline = false,
+      telescope = true,
+      treesitter = true,
+      vimwiki = false,
+      ["which-key"] = true,
+    },
+  },
+
+  diagnostics = { virtual_text = true, underline = true },
+
+  lsp = {
+    servers = {
+      -- "pyright"
+    },
+    formatting = {
+      format_on_save = {
+        enabled = true,
+      },
+      timeout_ms = 1000,
+    },
+    mappings = {
+      n = {
+        -- ["<leader>lf"] = false -- disable formatting keymap
+      },
+    },
+  },
+
+  mappings = {
+    n = {
+      -- mappings seen under group name "Buffer"
+      ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+      -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    },
+    t = {
+      -- setting a mapping to false will disable it
+      -- ["<esc>"] = false,
+    },
+  },
+
+  -- Configure plugins
+  plugins = {
+    init = {
+      { "tpope/vim-surround" },
+    },
+    ["null-ls"] = function(config)
+      -- local null_ls = require "null-ls"
+      -- config.sources = {
+      -- }
+      return config
+    end,
+    treesitter = {
+      highlight = { enable = true },
+      fold = { enable = true },
+      rainbow = { enable = true },
+    },
+    ["mason-lspconfig"] = { ensure_installed = { "sumneko_lua", "pyright" } },
+    ["mason-null-ls"] = {
+      ensure_installed = {
+        "stylua",
+        "luacheck",
+        "jq",
+        "yammlint",
+        "shellcheck",
+        "black",
+        "isort",
+        "mypy",
+      },
+    },
+    ["mason-nvim-dap"] = {
+      -- ensure_installed = { "python" },
+    },
+    zk = {
+      picker = "telescope",
+      lsp = {
+        config = {
+          cmd = { "zk", "lsp" },
+          name = "zk",
+          -- on_attach = ...
+        },
+        auto_attach = { enabled = true, filetypes = { "markdown" } },
+      },
+    },
+  },
+
+  luasnip = {
+    filetype_extend = {
+      -- javascript = { "javascriptreact" },
+    },
+    vscode = {
+      paths = {},
+    },
+  },
+
+  ["nvim-autopairs"] = {
+   map_c_h = true,
+   map_c_w = true,
+   map_bs = true,
+},
+
+  cmp = {
+    source_priority = {
+      nvim_lsp = 1000,
+      luasnip = 750,
+      buffer = 500,
+      path = 250,
+    },
+  },
+
+  -- Customize Heirline options
+  heirline = {
+    -- -- Customize different separators between sections
+    -- separators = {
+    --   tab = { "", "" },
+    -- },
+    -- -- Customize colors for each element each element has a `_fg` and a `_bg`
+    -- colors = function(colors)
+    --   colors.git_branch_fg = astronvim.get_hlgroup "Conditional"
+    --   return colors
+    -- end,
+    -- -- Customize attributes of highlighting in Heirline components
+    -- attributes = {
+    --   -- styling choices for each heirline element, check possible attributes with `:h attr-list`
+    --   git_branch = { bold = true }, -- bold the git branch statusline component
+    -- },
+    -- -- Customize if icons should be highlighted
+    -- icon_highlights = {
+    --   breadcrumbs = false, -- LSP symbols in the breadcrumbs
+    --   file_icon = {
+    --     winbar = false, -- Filetype icon in the winbar inactive windows
+    --     statusline = true, -- Filetype icon in the statusline
+    --   },
+    -- },
+  },
+
+  -- Modify which-key registration (Use this with mappings table in the above.)
+  ["which-key"] = {
+    -- Add bindings which show up as group name
+    register = {
+      -- first key is the mode, n == normal mode
+      n = {
+        -- second key is the prefix, <leader> prefixes
+        ["<leader>"] = {
+          -- third key is the key to bring up next level and its displayed
+          -- group name in which-key top level menu
+          ["b"] = { name = "Buffer" },
+        },
+      },
+    },
+  },
+
+  polish = function() end,
+}
+
+local _options = {
+  opt = {
+    relativenumber = false,
+    number = true,
+    spell = true,
+    signcolumn = "auto",
+    wrap = true,
+    foldenable = false,
+    foldexpr = "nvim_treesitter#foldexpr()",
+    foldmethod = "expr",
+  },
+  g = {
+    mapleader = " ",
+    -- lsp.formatting.format_on_save must be enabled
+    autoformat_enabled = true,
+    cmp_enabled = true,
+    autopairs_enabled = true,
+    diagnostics_enabled = true,
+    status_diagnostics_enabled = true,
+    icons_enabled = true,
+    ui_notifications_enabled = true,
+    heirline_bufferline = false,
+  },
+}
+
+local options = function(local_vim)
+  local_vim.opt.whichwrap = vim.opt.whichwrap + "l,h"
+
+  for k, v in pairs(_options) do
+    for sk, sv in pairs(v) do
+      local_vim[k][sk] = sv
+    end
+  end
+
+  return local_vim
 end
 
-astronvim.conditional_func(astronvim.user_plugin_opts("polish", nil, false))
-
-if vim.fn.has "nvim-0.8" ~= 1 or vim.version().prerelease then
-  vim.schedule(function() astronvim.notify("Unsupported Neovim Version! Please check the requirements", "error") end)
-end
+config.options = options
+return config
